f6c2332ee0360e542084d889d54c18688b3dcd0b
hamza-che <chenguitiha@gmail.com>
2021-07-19 22:18:54 +0100

    added footer to home page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const playBtn = document.querySelector(".play");
 const video = document.querySelector(".features .video video");
 
+// Map Functionality
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaGFtemFjaDk3IiwiYSI6ImNrcWxiZWNlcjN6M3kyd212MWZseWF4ZXcifQ.xwVPIODcdaYqfYW4NvGhyw";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [33.796296512357024, 2.8619866526413844],
+});
+
 // Features Video Functionality
 playBtn.addEventListener("click", function () {
   video.play();
